f4fdbd738e1a97133adaa825a72ecbb4d472e8d9
PavanVasist <pavanvasisht29@gmail.com>
2017-10-02 12:35:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool = new Pool(config);
-app.get('/test-db', function(req, result) {
+app.get('/test-db', function (req, result) {
     pool.query('SELECT * FROM test', function (err, result){
         if(err){
-            res.status(500).send(err.toStringify());
+            res.status(500).send(err.toString());
         } else{
             res.send(JSON.stringify(result));
         }
