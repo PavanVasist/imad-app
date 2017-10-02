0f0285b48eaf5511eb793b94b15ac29cf0651422
PavanVasist <pavanvasisht29@gmail.com>
2017-10-02 21:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/test-db', function (req, res) {
 });
 app.get('/articles/:articleName', function(req, res){
    var articleName = req.param.articleName;
-   pool.query("SELECT * FROM articles WHERE title = ' " + articleName + "'", function(err,result){
+   pool.query("SELECT * FROM articles WHERE title = '" + articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else {
