c176444703c0526af21682117de190ead7de9d1d
PavanVasist <pavanvasisht29@gmail.com>
2017-10-02 21:05:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,43 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+    var title=  data.title;
+    var date  =data.date;
+    var heading=  data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = ` 
+    <html>
+         <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class = 'container'>
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date.toDateString()}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+    
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -26,6 +63,22 @@ app.get('/test-db', function (req, res) {
             res.send(JSON.stringify(result.rows));
         }
     });
+});
+app.get('/articles/:articleName', function(req, res){
+   var articleName = req.param.articleName;
+   pool.query("SELECT * FROM articles where title = ' " + articleName + "'", function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length  === 0){
+               res.status(404).send("Article not found");
+           } else{
+               var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+           }
+       }
+   });
+  
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
